37ca256bda91c3501656ee1415ba0d090434bab4
Sinster69 <pranavjaganani@gmail.com>
2026-04-09 17:51:47 +0530

    Fourth Major Update

--- hrms-backend/database/connection.py
+++ hrms-backend/database/connection.py
@@ -2,9 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./hrms.db"
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = "mssql+pyodbc://@localhost\\SQLEXPRESS/HRMS?driver=ODBC+Driver+17+for+SQL+Server&TrustServerCertificate=yes"
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(bind=engine)
 
